246af1c9f48e724d3805b9f94a3268d5a6c29f90
Tyler Hatton <tyler.hatton@gmail.com>
2021-05-30 00:57:47 -0500

    working on template build process

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json, os
 from typing import Optional
 from fastapi import FastAPI, Form
 from slack import WebClient
-from modal import BigIpListModal, TemplateListModal
+from modal import BigIpListModal, TemplateListModal, TemplateModal
 from credentials import get_hosts, get_username, get_password
 
 app = FastAPI()
@@ -36,6 +36,19 @@ async def open_fast_prompt(payload: str = Form(...)):
         bigip_password = get_password(creds_file, bigip)
 
         modal.populate_template_list(bigip, bigip_username, bigip_password)
+
+        return({
+            "response_action": "push",
+            "view": modal.contents
+        })
+    elif json_payload['type'] == 'view_submission' and json_payload['view']['callback_id'] == 'template_list':
+        modal = TemplateModal()
+        bigip = json_payload['view']['blocks'][1]['text']['text']
+        template = json_payload['view']['state']['values']['template_select']['static_select-action']['selected_option']['value']
+        bigip_username = get_username(creds_file, bigip)
+        bigip_password = get_password(creds_file, bigip)
+
+        modal.populate_template(bigip, template, bigip_username, bigip_password)
 
         return({
             "response_action": "push",
--- modal.py
+++ modal.py
@@ -95,13 +95,13 @@ class TemplateListModal:
                     }
                 },
                 {
-                    "block_id": "bigip_select",
+                    "block_id": "template_select",
                     "type": "input",
                     "element": {
                         "type": "static_select",
                         "placeholder": {
                             "type": "plain_text",
-                            "text": "FAST Template",
+                            "text": "Select a FAST Template",
                             "emoji": True
                         },
                         "options": [],
@@ -109,7 +109,7 @@ class TemplateListModal:
                     },
                     "label": {
                         "type": "plain_text",
-                        "text": "Select a FAST Template",
+                        "text": "FAST Template",
                         "emoji": True
                     }
                 }
@@ -138,4 +138,93 @@ class TemplateListModal:
         
         self.contents['blocks'][1]['text']['text'] = bigip
         self.contents['blocks'][2]['element']['options'] = template_list
+        return
+
+class TemplateModal:
+    def __init__(self):
+        self.contents = {
+            "type": "modal",
+            "callback_id": "template_list",
+            "title": {
+                "type": "plain_text",
+                "text": "F5 FAST Template",
+                "emoji": True
+            },
+            "submit": {
+                "type": "plain_text",
+                "text": "Submit",
+                "emoji": True
+            },
+            "close": {
+                "type": "plain_text",
+                "text": "Back",
+                "emoji": True
+            },
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "*Target BIG-IP*"
+                    }
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Placeholder"
+                    }
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "*FAST Template*"
+                    }
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Placeholder"
+                    }
+                }
+            ]      
+        }
+
+    def populate_template(self, bigip, template, bigip_username, bigip_password):
+        self.contents['blocks'][1]['text']['text'] = bigip
+        self.contents['blocks'][3]['text']['text'] = template
+
+        response = requests.get(
+            'https://' + bigip + '/mgmt/shared/fast/templates/' + template,
+            auth=(bigip_username, bigip_password),
+            verify=False,
+            timeout=10
+        )
+
+        template_properties = response.json()['_parametersSchema']['properties']
+
+        for template in template_properties.keys():
+            template_type = template_properties[template]['type']
+            template_default = str(template_properties[template].get('default')) or ' '
+
+            if template_type == 'string' or template_type == 'integer':
+                self.contents['blocks'].append(
+                    {
+                        "type": "input",
+                        "element": {
+                            "type": "plain_text_input",
+                            "action_id": template,
+                            "placeholder": {
+                                "type": "plain_text",
+                                "text": str(template_default)
+                            }
+                        },
+                        "label": {
+                            "type": "plain_text",
+                            "text": template
+                        }
+                    } 
+                )
         return
